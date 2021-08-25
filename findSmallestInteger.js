6cb09ec1fa007edc8e592251ba98ea7f5ffe24c9
Jack Schollmeyer <jack@schollmeyer.com>
2021-08-25 10:29:53 -0500

    This funciton takes in an array and returns the smallest integer of that array

---
+++ findSmallestInteger.js
@@ -0,0 +1,7 @@
+//This funciton takes in an array and returns the smallest integer of that array
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args);
+    }
+  }
