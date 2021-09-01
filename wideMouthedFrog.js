114ec4a40871bd5538c4f1a031dd137dcc1ee3e4
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-01 11:36:45 -0500

    This function returns small if the animal encounterd is an alligator and wide otherwise

---
+++ wideMouthedFrog.js
@@ -0,0 +1,5 @@
+//This function returns small if the animal encounterd is an alligator and wide otherwise
+
+function mouthSize(animal) {
+    return animal.toLowerCase() == "alligator" ? "small" : "wide";
+   }
