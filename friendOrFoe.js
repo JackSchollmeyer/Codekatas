e2f7ce94b0c56d35633988c0deade7d69f60c187
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-03 17:18:21 -0500

    This function will check if the first argument's string ends with what the second argument's input is

--- friendOrFoe.js
+++ friendOrFoe.js
@@ -1,4 +1,4 @@
-//This function checks which names are 4 letters long in an array of strings 
+//This function checks which names are 4 letters long in an array of strings
 
 function friend(friends){
     return friends.filter(name => name.length === 4)
---
+++ stringEndsWith.js
@@ -0,0 +1,5 @@
+//This function will check if the first argument's string ends with what the second argument's input is
+
+function solution(str, ending){
+    return str.endsWith(ending) ? true : false
+  }
