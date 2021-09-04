b2201a86a61422cdbd744f25a9cdad03c115e3b1
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-04 08:17:26 -0500

    This function checks which quarter the month number belongs to.

---
+++ quarterOfTheYear.js
@@ -0,0 +1,16 @@
+//This function checks which quarter the month number belongs to. 
+
+function quarterOf(month) {
+    if(month === 1) {return 1}else
+    if(month === 2) {return 1}else
+    if(month === 3) {return 1}else
+    if(month === 4) {return 2}else
+    if(month === 5) {return 2}else
+    if(month === 6) {return 2}else
+    if(month === 7) {return 3}else
+    if(month === 8) {return 3}else
+    if(month === 9) {return 3}else
+    if(month === 10) {return 4}else
+    if(month === 11) {return 4}else
+    if(month === 12) {return 4};
+  }
