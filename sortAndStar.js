39447e6c12b6b1302a7e4dc90049e50b959377c9
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-06 18:35:48 -0500

    The following function takes the alphabetically first string in an array and returns it with 3 stars between each letter

--- sortAndStar.js
+++ sortAndStar.js
@@ -1,5 +1,5 @@
 //The following function takes the alphabetically first string in an array
-//and returns it with 3 staras between each letter
+//and returns it with 3 stars between each letter
 
 function twoSort(s) {
     return s.sort().slice(0, 1).toString().split('').join('***')
