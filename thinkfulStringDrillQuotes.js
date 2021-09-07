93d8ea8aeefe6985f3e611fec12d13e2533ead2e
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-07 08:31:29 -0500

    This function will return a person's name and their quote with some extra words/charecters included

---
+++ thinkfulStringDrillQuotes.js
@@ -0,0 +1,5 @@
+//This function will return a person's name and their quote with some extra words/charecters included
+
+function quotable(name, quote) {
+    return  name + ' said: "' + quote + '"'
+  }
