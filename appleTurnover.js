a25909cdbdf4dd364d18990212820449b2fd2078
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-08 12:52:02 -0500

    This function squares the input(x) and returns one of the strings depending on if it's greater than 1000 or not

---
+++ appleTurnover.js
@@ -0,0 +1,5 @@
+//This function squares the input(x) and returns one of the strings depending on if it's greater than 1000 or not
+
+function apple(x){
+    return x * x > 1000 ? "It's hotter than the sun!!" : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
