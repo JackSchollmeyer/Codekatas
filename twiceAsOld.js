5008cfa3829219413b5a7965e2040aeb330f9539
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-10 07:38:26 -0500

    This function will takes 2 ages(with the first being the older one) and calculate in how long or how long ago the older was twice the age of the younger

---
+++ twiceAsOld.js
@@ -0,0 +1,6 @@
+//This function will takes 2 ages(with the first being the older one)
+//and calculate in how long or how long ago the older was twice the age of the younger 
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - sonYearsOld * 2)
+  }
