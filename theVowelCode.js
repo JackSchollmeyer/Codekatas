763f9db2218ad96d7628511dc768cbba6237c95f
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-13 08:53:02 -0500

    This kata has two funcitons. One converts numbers to the corresponding vowels and the other does the opposite.

---
+++ theVowelCode.js
@@ -0,0 +1,8 @@
+//This kata has two funcitons. One converts numbers to the corresponding vowels and the other does the opposite.
+
+function encode(string) {
+    return string.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4).replace(/u/g, 5)
+  }
+  function decode(string) {
+    return string.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u')
+  }
