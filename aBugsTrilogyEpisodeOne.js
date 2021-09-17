cfe1c73e9d472fe403b71076976f11a10061811e
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-17 10:49:36 -0500

    I had to fix the syntax of this code to get it to work properly

---
+++ aBugsTrilogyEpisodeOne.js
@@ -0,0 +1,8 @@
+//I had to fix the syntax of this code to get it to work properly
+
+function yourFutureCareer() {
+  var career = Math.random()
+  if (career <= 0.32) { return 'FrontEnd Developer'}
+  else if (career <= 0.65) { return 'BackEnd Developer'}
+  else { return 'Full-Stack Developer'}
+}
