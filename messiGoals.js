6aa9a83967592916b87d747130f2d57ee212b17b
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-20 16:50:45 -0500

    This code shows how many goals Messi scored in 3 competitions and the sum of them

---
+++ messiGoals.js
@@ -0,0 +1,6 @@
+//This code shows how many goals Messi scored in 3 competitions and the sum of them
+
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+let totalGoals = 58
