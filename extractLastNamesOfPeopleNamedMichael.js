2c11fcbf5d9be3807186f8c443d950fb282ac1b6
Jack Schollmeyer <jack@schollmeyer.com>
2021-09-22 09:37:40 -0500

    This function will take in a string and return an array of the last names of anyone names 'Michael'

---
+++ extractLastNamesOfPeopleNamedMichael.js
@@ -0,0 +1,10 @@
+//This function will take in a string and return an array of the last names of anyone names 'Michael'
+
+function getMichaelLastName(inputText) {
+    let arr = [];
+    let list = inputText.replace(/[?.,]/g, '').split(' ');
+    for (let i = 0; i < list.length; i++) {
+      if (list[i - 1] == 'Michael' && /^[A-Z]/.test(list[i])) { arr.push(list[i])}
+    }
+    return arr  
+  }
