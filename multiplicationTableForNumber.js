f92b023f8941319cac74ae0aa8cc6321d636d45b
Jack Schollmeyer <jack@schollmeyer.com>
2021-10-07 08:02:02 -0500

    This function returns a string of each number from 1 to 10 being multiplied by number

---
+++ multiplicationTableForNumber.js
@@ -0,0 +1,56 @@
+//This function returns a string of each number from 1 to 10 being multiplied by number
+
+function multiTable(number) {
+  let a = 1 * number;
+  let b = 2 * number;
+  let c = 3 * number;
+  let d = 4 * number;
+  let e = 5 * number;
+  let f = 6 * number;
+  let g = 7 * number;
+  let h = 8 * number;
+  let i = 9 * number;
+  let j = 10 * number;
+  return (
+    "1 * " +
+    number +
+    " = " +
+    a +
+    "\n2 * " +
+    number +
+    " = " +
+    b +
+    "\n3 * " +
+    number +
+    " = " +
+    c +
+    "\n4 * " +
+    number +
+    " = " +
+    d +
+    "\n5 * " +
+    number +
+    " = " +
+    e +
+    "\n6 * " +
+    number +
+    " = " +
+    f +
+    "\n7 * " +
+    number +
+    " = " +
+    g +
+    "\n8 * " +
+    number +
+    " = " +
+    h +
+    "\n9 * " +
+    number +
+    " = " +
+    i +
+    "\n10 * " +
+    number +
+    " = " +
+    j
+  );
+}
