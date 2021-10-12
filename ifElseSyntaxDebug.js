032cddd562f18345a6f1080636a3da0dac9c2709
Jack Schollmeyer <jack@schollmeyer.com>
2021-10-12 07:24:44 -0500

    In the following problem I had to fix the format of the if/else statement

---
+++ ifElseSyntaxDebug.js
@@ -0,0 +1,9 @@
+//In the following problem I had to fix the format of the if/else statement
+
+function checkAlive(health) {
+  if (health <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
