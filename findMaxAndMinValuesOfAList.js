4500e163ebc9eb52f8957067de3da289c082689b
Jack Schollmeyer <jack@schollmeyer.com>
2021-10-22 10:18:45 -0500

    The following function will find the maximum and minumum values of a list

---
+++ findMaxAndMinValuesOfAList.js
@@ -0,0 +1,9 @@
+//The following function will find the maximum and minumum values of a list
+
+var min = function (list) {
+  return Math.min(...list);
+};
+
+var max = function (list) {
+  return Math.max(...list);
+};
